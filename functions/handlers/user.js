e6f4a5dcf86e0c3680622caa8620736e031d202a
ariefirawan <ariefirawan@users.noreply.github.com>
2020-04-01 20:00:10 +0700

    add upload image function

--- functions/handlers/user.js
+++ functions/handlers/user.js
@@ -1,6 +1,6 @@
 const firebase = require('firebase');
 
-const { db } = require('../util/admin');
+const { admin, db } = require('../util/admin');
 const configFirebase = require('../util/config');
 const {
   validateSignInData,
@@ -16,6 +16,8 @@ exports.signup = (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   };
+
+  const noImg = 'blank-profile.png';
 
   const { valid, errors } = validateSignupData(newUser);
 
@@ -43,6 +45,7 @@ exports.signup = (req, res) => {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${configFirebase.storageBucket}/o/${noImg}?alt=media`,
         userId: userIdSnap
       };
       return db.doc(`/users/${newUser.handle}`).set(userCredentials);
@@ -80,6 +83,63 @@ exports.signIn = (req, res) => {
       return res
         .status(403)
         .json({ general: 'Wrong credentials, please try again' });
-    })
-    
+    });
+};
+
+exports.uploadImage = (req, res) => {
+  const Busboy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
+
+  const busboy = new Busboy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    if (
+      mimetype !== 'image/jpeg' &&
+      mimetype !== 'image/jpg' &&
+      mimetype !== 'image/png'
+    ) {
+      return res.status(400).json({ error: 'Wrong file submited' });
+    }
+    console.log(fieldname);
+    console.log(filename);
+    console.log(mimetype);
+
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on('finish', () => {
+    admin
+      .storage()
+      .bucket(configFirebase.storageBucket)
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype
+          }
+        }
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${configFirebase.storageBucket}/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: 'Image uploaded successfully' });
+      })
+      .catch(err => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,17 @@ const functions = require('firebase-functions');
 const express = require('express');
 
 const auth = require('./util/auth');
-const { getAllScreams, addScream } = require('./handlers/scream');
-const { signup, signIn } = require('./handlers/user');
+const posts = require('./handlers/scream');
+const userHandler = require('./handlers/user');
 
 const app = express();
 
-app.get('/screams', getAllScreams);
+app.get('/screams', posts.getAllScreams);
+app.post('/scream', auth, posts.addScream);
 
-app.post('/scream', auth, addScream);
-
-app.post('/signup', signup);
-
-app.post('/login', signIn);
+app.post('/signup', userHandler.signup);
+app.post('/login', userHandler.signIn);
+app.post('/imageUpload', auth, userHandler.uploadImage);
 
 //https://baseurl.com/api
 exports.api = functions.https.onRequest(app);
